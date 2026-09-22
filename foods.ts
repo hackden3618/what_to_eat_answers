919379def05e3dd62cf456ab376e028b1b4abad3
hackden3618 <denniswambugu@protonmail.com>
2026-09-22 08:25:54 +0300

    initialized and built the simple first version focused on one person only

---
+++ foods.ts
@@ -0,0 +1,41 @@
+const bree_foods_ugali = {
+    1: "ugali_beef",
+    2: "ugali_pork",
+    3: "ugali_sagaa",
+    4: "ugali_misheveve",
+    5: "ugali_mala",
+    6: "ugali_mayai",
+    7: "ugali_matumbo",
+    8: "ugali_maini",
+    9: "ugali_chicken",
+    10: "ugali_managu",
+    11: "ugali_sossi",
+    12: "ugali_mchicha_spinach",
+    13: "ugali_mchicha",
+    14: "ugali_omena",
+}
+const bree_foods_rice = {
+    1: "rice_beef",
+    2: "rice_beans",
+    3: "rice_sossi",
+    4: "rice_ndengu_special",
+    5: "rice_matoke",
+    6: "rice_chicken",
+    7: "rice_viazi",
+    8: "rice_kamande",
+    9: "pilau",
+}
+const bree_foods_chapati = {
+    1: "chapati_beef",
+    2: "chapati_chicken",
+    3: "chapati_beans",
+    4: "chapati_viazi_beans",
+    5: "chapati_ndengu_special",
+    6: "chapati_kamande",
+}
+// const specials = {
+//     1: bree_foods_ugali.[9],
+//     2: bree_foods_rice.[6],
+//     3: bree_foods_chapati[1],
+//     3: bree_foods_chapati[2],
+// }
